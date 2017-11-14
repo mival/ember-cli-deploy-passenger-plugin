fb177bc654cb6549aad1d74b9b21d2c61e6eca12
Jan Voňavka <jan.vonavka@gmail.com>
2017-11-14 14:21:50 +0100

    mv -> cp

--- index.js
+++ index.js
@@ -195,7 +195,8 @@ module.exports = {
           _this.log('Moving...');
           // symlink rsyncPath ../../shared/assets
           // mv assets/* /home/domena.cz/apps/frontend/shared/assets
-          var cmd = `mv ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets', '/', '*') } ${path.posix.join(_this.readConfig('path'), '/', 'shared', '/', 'assets')} && ` +
+          var cmd = `cp ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets', '/', '*') } ${path.posix.join(_this.readConfig('path'), '/', 'shared', '/', 'assets')} && ` +
+            `mv ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets') } ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets_bak') } && ` +
             `rm -r ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets') } && ` +
             `ln -s ${path.posix.join(_this.readConfig('path'), '/', 'shared', '/', 'assets')} ${path.posix.join(_this.readConfig('path'), 'releases', '/', revisionKey, '/', 'assets') }`;
           return _this._execCmd(cmd, function() {
